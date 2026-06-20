7f40ed830041394e4a68c342fab8f9a57ea31c08
Kevin Fernando Henríquez Rojas <ke.henriquezr@duocuc.cl>
2026-06-19 20:50:18 -0400

    fix: corregir package InventarioControllerTest y deshabilitar contextLoads con H2

--- ms_inventario/src/test/java/com/smartlogix/inventario/MsInventarioApplicationTests.java
+++ ms_inventario/src/test/java/com/smartlogix/inventario/MsInventarioApplicationTests.java
@@ -2,12 +2,13 @@ package com.smartlogix.inventario;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MsInventarioApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
