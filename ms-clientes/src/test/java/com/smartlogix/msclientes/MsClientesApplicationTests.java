750020e23a28e115de45c6af980421e20ff9c43a
Kevin Fernando Henríquez Rojas <ke.henriquezr@duocuc.cl>
2026-06-19 20:25:36 -0400

    feat: Testeo v1 microservicios

--- ms-clientes/src/test/java/com/smartlogix/msclientes/MsClientesApplicationTests.java
+++ ms-clientes/src/test/java/com/smartlogix/msclientes/MsClientesApplicationTests.java
@@ -2,9 +2,12 @@ package com.smartlogix.msclientes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")   // ← ¿está esta línea?
 class MsClientesApplicationTests {
+
     @Test
     void contextLoads() {
     }
--- ms-ventas/src/test/java/com/smartlogix/msventas/MsVentasApplicationTests.java
+++ ms-ventas/src/test/java/com/smartlogix/msventas/MsVentasApplicationTests.java
@@ -2,10 +2,11 @@ package com.smartlogix.msventas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MsVentasApplicationTests {
-
     @Test
     void contextLoads() {
     }
